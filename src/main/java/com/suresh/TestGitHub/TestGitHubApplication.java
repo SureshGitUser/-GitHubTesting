0d9f987fd4ab6d00d85cca8c0427e195b25c2ab3
SureshGitUser <sureshisworking@gmail.com>
2025-05-24 19:14:26 +0530

    first push

---
+++ src/main/java/com/suresh/TestGitHub/TestGitHubApplication.java
@@ -0,0 +1,14 @@
+package com.suresh.TestGitHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestGitHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestGitHubApplication.class, args);
+		System.out.println("welcome to Gradle!");
+	}
+
+}
---
+++ src/main/java/com/suresh/TestGitHub/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package com.suresh.TestGitHub.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello, this is the root URL!";
+    }
+}
